bff371de91997103c9a7cb65f0c903f7f3e33e2e
and00h <antonio.porsia00@gmail.com>
2022-02-24 16:33:13 +0100

    - Fixed CGB sprite OAM priority bug

--- inc/Core/Graphics/CGBPalette.h
+++ inc/Core/Graphics/CGBPalette.h
@@ -5,22 +5,21 @@
 #ifndef OHBOI_CGBPALETTE_H
 #define OHBOI_CGBPALETTE_H
 
-
-#include <SDL2/SDL_types.h>
+#include <stdint.h>
 
 class CGBPalette {
 public:
     CGBPalette();
     ~CGBPalette() = default;
 
-    Uint32 get_color(int palette_number, int color_number);
+    uint32_t get_color(int palette_number, int color_number);
     uint8_t get_byte(int index);
     void set_byte(uint8_t val, int index);
-    Uint32* get_palette(int palette_number);
+    uint32_t* get_palette(int palette_number);
     void update(int palette_number);
 private:
-    uint8_t val[64];
-    Uint32 palettes[8][4];
+    uint8_t values[64];
+    uint32_t palettes[8][4];
 };
 
 #endif //OHBOI_CGBPALETTE_H
--- src/Core/Memory/Dma_controller.cpp
+++ src/Core/Memory/Dma_controller.cpp
@@ -25,25 +25,28 @@ namespace gb::memory {
         dma_trigger_ = true;
     }
 
-    void Memory::Dma_controller::step(int cycles) {
+    void Memory::Dma_controller::step(unsigned int cycles) {
         static const uint8_t dma_size = 0xA0;
+        int _c = static_cast<int>(cycles);
 
-        while ( cycles > 0 && !dma_completed_ ) {
-            if ( dma_trigger_ ) {
+        if ( dma_trigger_ ) {
+            dma_trigger_ = false;
+            dma_wait_ = true;
+            _c -= 4;
+        }
+
+        if ( dma_wait_ && _c > 0 ) {
+            dma_wait_ = false;
+            _c = -4;
+        }
+
+        while ( _c > 0 && !dma_completed_ ) {
+            mem_.dma_write_oam(dma_index_, mem_.read(dma_addr_ + dma_index_));
+            if (++dma_index_ == dma_size ) {
                 dma_trigger_ = false;
-                dma_wait_ = true;
-                cycles -= 4;
-                continue;
+                dma_completed_ = true;
             }
-            if ( !dma_wait_ ) {
-                mem_.dma_write_oam(dma_index_, mem_.read(dma_addr_ + dma_index_));
-                if (++dma_index_ == dma_size ) {
-                    dma_trigger_ = false;
-                    dma_completed_ = true;
-                }
-            } else
-                dma_wait_ = false;
-            cycles -= 4;
+            _c -= 4;
         }
     }
 }
